6475086d3c1a489159dca133f01e22df694b4bbb
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-08-01 20:29:15 +0700

    Add west java logo

---
+++ components/Header/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="bg-green-800 h-20 flex items-center relative">
+    <div class="container">
+      <Navigation />
+    </div>
+  </header>
+</template>
---
+++ components/Navigation/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="flex items-center">
+    <MenuLink link="/" class="mr-auto">
+      <img src="/logo.png" width="55" height="60" alt="Beranda">
+    </MenuLink>
+  </nav>
+</template>
