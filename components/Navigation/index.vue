797fb3071b64c852b694c7278a41cfeddcb9a6c8
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-08-02 09:07:33 +0700

    Add public service menu

--- components/Navigation/index.vue
+++ components/Navigation/index.vue
@@ -3,5 +3,87 @@
     <MenuLink link="/" class="mr-auto">
       <img src="/logo.png" width="55" height="60" alt="Beranda">
     </MenuLink>
+    <Menu>
+      <MenuList
+        v-for="menu in menus"
+        :id="menu.id"
+        :key="menu.id"
+        :title="menu.title"
+        @click="setActiveMenu"
+      >
+        <MenuItem
+          :id="menu.id"
+          :title="menu.title"
+          :link="menu.link"
+          :items="menu.items"
+          :active-menu="activeMenu"
+          @click="setActiveMenu"
+        />
+      </MenuList>
+    </Menu>
   </nav>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      menus: [
+        {
+          id: 1,
+          title: 'Layanan Publik',
+          link: '#',
+          items: [
+            {
+              id: 1,
+              title: 'Kependudukan',
+              description: 'Semakin mudah akses layanan urusan identitas hingga akta.',
+              icon: '/icons/kependudukan.svg',
+              link: 'http://disdukcapil.jabarprov.go.id/portal',
+              items: []
+            },
+            {
+              id: 2,
+              title: 'Kepolisian',
+              description: 'Cari tahu informasi mengenai layanan kepolisian terlengkap.',
+              icon: '/icons/kepolisian.svg',
+              link: 'https://www.polri.go.id/',
+              items: []
+            },
+            {
+              id: 3,
+              title: 'Pajak',
+              description: 'Administrasi perpajakan lebih mudah dan cepat.',
+              icon: '/icons/pajak.svg',
+              link: 'https://bapenda.jabarprov.go.id/',
+              items: []
+            },
+            {
+              id: 4,
+              title: 'Perizinan',
+              description: 'Berikan kemudahan perizinan berbagai kebutuhan.',
+              icon: '/icons/perizinan.svg',
+              link: 'https://dpmptsp.jabarprov.go.id/sicantik/main/pendaftaranbaru/perizinanonline',
+              items: []
+            },
+            {
+              id: 5,
+              title: 'Pendidikan dan Pembelajaran',
+              description: 'Pemukan secara menyeluruh informasi mengenai pendidikan.',
+              icon: '/icons/pendidikan-dan-pembelajaran.svg',
+              link: 'http://disdik.jabarprov.go.id/',
+              items: []
+            }
+          ]
+        }
+      ],
+      activeMenu: null
+    }
+  },
+  methods: {
+    setActiveMenu (id) {
+      this.activeMenu = id
+    }
+  }
+}
+</script>
