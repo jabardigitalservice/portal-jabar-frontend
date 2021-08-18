b40c40afbb01d150f60b0b2104307e08a6ea38ac
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-08-18 08:43:16 +0700

    Add todo related to jabar design system

--- components/Hero/index.vue
+++ components/Hero/index.vue
@@ -10,12 +10,11 @@
       <p class="text-xl text-gray-800 leading-relaxed text-center max-w-2xl">
         Di sini kamu bisa akses informasi, layanan publik, dan sampaikan aspirasi mengenai program dari Pemerintah Daerah Provinsi Jawa Barat.
       </p>
+      <!--
+        TODO: Replace search input with search component from jabar design system
+      -->
       <form class="flex py-2 pr-2 pl-3 border border-gray-500 rounded-lg w-3/5" tabindex="0">
         <input type="text" class="border-none flex-grow focus:outline-none" placeholder="Cari disini">
-        <!--
-        @todo
-        use button from jabar design system
-        -->
         <button type="button" class="bg-green-800 px-4 py-2 rounded-lg text-white text-sm font-bold">
           Cari
         </button>
