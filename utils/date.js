28bfcae34adc1cc4bd86a5049e58fe69f4e7f667
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-08-30 09:55:07 +0700

    Add date iso format utils

--- utils/date.js
+++ utils/date.js
@@ -1,3 +1,11 @@
 export function format (date, options) {
   return new Date(date).toLocaleDateString('id-ID', options)
 }
+
+export function toISO (date, time = false) {
+  if (time) {
+    return new Date(date).toISOString()
+  }
+
+  return new Date(date).toISOString().substring(0, 10)
+}
