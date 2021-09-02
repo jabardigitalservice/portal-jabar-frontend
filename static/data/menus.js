87f36c85655c1ca6d326184142ec2caa97a08236
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-09-02 14:43:40 +0700

    Change icons directory

--- static/data/menus.js
+++ static/data/menus.js
@@ -8,7 +8,7 @@ export const navigationMenu = [
         id: 1,
         title: 'Kependudukan',
         description: 'Semakin mudah akses layanan urusan identitas hingga akta.',
-        icon: '/icons/kependudukan.svg',
+        icon: '/icons/layanan-publik/kependudukan.svg',
         link: 'http://disdukcapil.jabarprov.go.id/portal',
         items: []
       },
@@ -16,7 +16,7 @@ export const navigationMenu = [
         id: 2,
         title: 'Kepolisian',
         description: 'Cari tahu informasi mengenai layanan kepolisian terlengkap.',
-        icon: '/icons/kepolisian.svg',
+        icon: '/icons/layanan-publik/kepolisian.svg',
         link: 'https://www.polri.go.id/',
         items: []
       },
@@ -24,7 +24,7 @@ export const navigationMenu = [
         id: 3,
         title: 'Pajak',
         description: 'Administrasi perpajakan lebih mudah dan cepat.',
-        icon: '/icons/pajak.svg',
+        icon: '/icons/layanan-publik/pajak.svg',
         link: 'https://bapenda.jabarprov.go.id/',
         items: []
       },
@@ -32,7 +32,7 @@ export const navigationMenu = [
         id: 4,
         title: 'Perizinan',
         description: 'Berikan kemudahan perizinan berbagai kebutuhan.',
-        icon: '/icons/perizinan.svg',
+        icon: '/icons/layanan-publik/perizinan.svg',
         link: 'https://dpmptsp.jabarprov.go.id/sicantik/main/pendaftaranbaru/perizinanonline',
         items: []
       },
@@ -40,7 +40,7 @@ export const navigationMenu = [
         id: 5,
         title: 'Pendidikan dan Pembelajaran',
         description: 'Pemukan secara menyeluruh informasi mengenai pendidikan.',
-        icon: '/icons/pendidikan-dan-pembelajaran.svg',
+        icon: '/icons/layanan-publik/pendidikan-dan-pembelajaran.svg',
         link: 'http://disdik.jabarprov.go.id/',
         items: []
       }
@@ -199,21 +199,21 @@ export const featuredProgramMenu = [
     id: 1,
     title: 'Sapawarga',
     description: 'Temukan informasi dan akses layanan tentang Organisasi Perangkat Daerah di Jawa Barat',
-    icon: '/icons/sapawarga.svg',
+    icon: '/icons/program-unggulan/sapawarga.svg',
     link: 'https://digitalservice.jabarprov.go.id/sapawarga'
   },
   {
     id: 2,
     title: 'Desa Digital',
     description: 'Temukan informasi dan akses layanan tentang Organisasi Perangkat Daerah di Jawa Barat',
-    icon: '/icons/desa-digital.svg',
+    icon: '/icons/program-unggulan/desa-digital.svg',
     link: 'https://digitalservice.jabarprov.go.id/index.php/desa-digital-jabar'
   },
   {
     id: 3,
     title: 'Ekosistem Data Jabar',
     description: 'Temukan informasi dan akses layanan tentang Organisasi Perangkat Daerah di Jawa Barat',
-    icon: '/icons/ekosistem-data-jabar.svg',
+    icon: '/icons/program-unggulan/ekosistem-data-jabar.svg',
     link: 'http://data.jabarprov.go.id'
   }
 ]
@@ -223,56 +223,56 @@ export const quickLinkMenu = [
     id: 1,
     title: 'Kependudukan',
     description: 'Ketahui lebih lanjut tentang semua hal hebat yang ditawarkan Jawa Barat.',
-    icon: '/icons/kependudukan-icon.svg',
+    icon: '/icons/akses-cepat/kependudukan.svg',
     link: 'http://disdukcapil.jabarprov.go.id/portal'
   },
   {
     id: 2,
     title: 'PPID Jabar',
     description: 'Pejabat Pengelola Informasi dan Dokumentasi Jawa Barat.',
-    icon: '/icons/ppid-jabar.svg',
+    icon: '/icons/akses-cepat/ppid-jabar.svg',
     link: 'http://ppid.jabarprov.go.id'
   },
   {
     id: 3,
     title: 'Sasaran Kinerja Pegawai',
     description: 'Aplikasi penilaian secara daring kinerja dan prestasi Pegawai Negeri Sipil.',
-    icon: '/icons/sasaran-kinerja-pegawai.svg',
+    icon: '/icons/akses-cepat/sasaran-kinerja-pegawai.svg',
     link: 'https://kinerja.jabarprov.go.id/login'
   },
   {
     id: 4,
     title: 'Info Harga Pangan',
     description: 'Dapatkan info harga rata-rata komoditas harian di Jawa Barat.',
-    icon: '/icons/info-harga-pangan.svg',
+    icon: '/icons/akses-cepat/info-harga-pangan.svg',
     link: 'http://priangan.org'
   },
   {
     id: 5,
     title: 'E-Samsat',
     description: 'Cara mudah bayar pajak kendaraan secara daring.',
-    icon: '/icons/e-samsat.svg',
+    icon: '/icons/akses-cepat/e-samsat.svg',
     link: 'https://bapenda.jabarprov.go.id/e-samsat-jabar'
   },
   {
     id: 6,
     title: 'Satu Data',
     description: 'Tata kelola data antar OPD di lingkungan Pemerintah Daerah Provinsi Jawa Barat.',
-    icon: '/icons/satu-data.svg',
+    icon: '/icons/akses-cepat/satu-data.svg',
     link: 'https://satudata.jabarprov.go.id/auth/login?returnUrl=%2Fhome'
   },
   {
     id: 7,
     title: 'Saber Pungli',
     description: 'Laporkan aktivitas Pungutan Liar (Pungli) di sini!',
-    icon: '/icons/saber-pungli.svg',
+    icon: '/icons/akses-cepat/saber-pungli.svg',
     link: 'http://saberpungli.jabarprov.go.id'
   },
   {
     id: 8,
     title: 'Lapor',
     description: 'Layanan aspirasi dan pengaduan daring warga Jawa Barat.',
-    icon: '/icons/lapor.svg',
+    icon: '/icons/akses-cepat/lapor.svg',
     link: 'https://www.lapor.go.id'
   }
 ]
