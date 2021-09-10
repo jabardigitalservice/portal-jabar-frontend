9c17cee0393e40e1ab44e66c936adc7e2c027e91
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-09-10 18:58:26 +0700

    Redesign featured program section

---
+++ components/Button/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <JdsButton :variant="variant" :type="type">
+    <slot />
+  </JdsButton>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: {
+      type: String,
+      required: false,
+      default: 'primary'
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'button'
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  font-size: 14px;
+  font-weight: 700;
+}
+.jds-button--tertiary-paddingless {
+  padding: 0;
+}
+.jds-button--tertiary-paddingless:hover {
+  background-color: inherit;
+}
+.jds-button--tertiary-paddingless:focus {
+  box-shadow: inherit;
+}
+</style>
