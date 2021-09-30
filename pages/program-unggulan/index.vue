c6f8fd9e84a711f79845deb98d83fd5d47c26059
Ibwedagama <wardana.wedagama@gmail.com>
2021-09-30 13:01:11 +0700

    chore (featured-program): add jumbotron component

--- pages/program-unggulan/index.vue
+++ pages/program-unggulan/index.vue
@@ -1,11 +1,20 @@
 <template>
   <main>
-    Program Unggulan
+    <Jumbotron v-bind="jumbotron" />
   </main>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      jumbotron: {
+        title: 'Program Unggulan',
+        subtitle: 'Dapatkan informasi mengenai program-program unggulan dari Pemerintah Daerah Provinsi Jawa Barat.',
+        backgroundImage: '/image/banner/1.webp'
+      }
+    }
+  }
 
 }
 </script>
