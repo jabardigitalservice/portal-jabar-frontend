bad9fce8de28e8b01c2796118b909f264335560f
Ibwedagama <wardana.wedagama@gmail.com>
2021-10-01 11:15:45 +0700

    chore (featured-program): change jumbotron image url

--- pages/tentang-jawa-barat/program-unggulan/index.vue
+++ pages/tentang-jawa-barat/program-unggulan/index.vue
@@ -15,7 +15,7 @@ export default {
       jumbotron: {
         title: 'Program Unggulan',
         subtitle: 'Dapatkan informasi mengenai program-program unggulan dari Pemerintah Daerah Provinsi Jawa Barat.',
-        backgroundImageUrl: '/images/banners/1.webp'
+        backgroundImageUrl: '/images/jumbotron/default.webp'
       }
     }
   }
