32d4878db06985668f7b443ca5b8f78c7d452f9a
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-10-06 09:15:49 +0700

    fix (agenda): add more padding top

--- components/Agenda/index.vue
+++ components/Agenda/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative -top-20 z-10">
+  <div class="relative -top-24 z-10">
     <div class="container mx-auto">
       <div class="bg-white p-10 rounded-xl shadow flex flex-col gap-7">
         <AgendaCalendar />
