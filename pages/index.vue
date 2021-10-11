b5fd2bde9e729b3aa299ab493d0a2c8d4f6b9c57
Ibwedagama <wardana.wedagama@gmail.com>
2021-10-11 15:30:29 +0700

    refactor (home): rename featured-program component; import featured-program-preview

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <main class="overflow-hidden">
     <Hero />
     <MostPopularServices />
-    <FeaturedProgram />
+    <FeaturedProgramPreview />
     <section class="pt-6 pb-10">
       <div class="container mx-auto">
         <div class="grid grid-cols-7 gap-8">
