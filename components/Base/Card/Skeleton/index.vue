b4b292a0215562c70662b7e8b12e33e824902389
Ibwedagama <wardana.wedagama@gmail.com>
2021-10-12 12:20:20 +0700

    chore (base/card): create skeleton component

---
+++ components/Base/Card/Skeleton/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="w-full bg-white py-3 px-4 group"
+  >
+    <div
+      class="inline-flex w-10 h-10 p-1 items-center justify-center mb-1
+      rounded-full bg-gray-200 animate-pulse"
+    />
+    <div class="w-3/4 h-6 bg-gray-200 animate-pulse mb-3 rounded-lg" />
+    <div class="w-full h-4 bg-gray-200 animate-pulse mb-2 rounded-lg" />
+    <div class="w-11/12 h-4 bg-gray-200 animate-pulse mb-2 rounded-lg" />
+  </div>
+</template>
