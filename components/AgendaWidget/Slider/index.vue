759b72ebf9898b07131fabce6f5f97d3fd684374
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-10-13 19:21:35 +0700

    fix(agenda widget): fix the last navigation icon on slider issue

--- components/AgendaWidget/Slider/index.vue
+++ components/AgendaWidget/Slider/index.vue
@@ -75,7 +75,17 @@ export default {
       return this.currentIndex === 0
     },
     lastSlide () {
-      return this.currentIndex === 3
+      /**
+       *  Current index is the first index on the displayed slide,
+       *  by adding 4 to the index, it will be the last index on the displayed slide.
+       */
+      const currentIndex = this.currentIndex + 4
+
+      if (this.items.length < 5) {
+        return true
+      }
+
+      return currentIndex === this.items.length
     }
   },
   mounted () {
