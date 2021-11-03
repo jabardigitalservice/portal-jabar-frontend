de349694c63037b6620b9b2f8f31d4734b36ee09
Ibwedagama <wardana.wedagama@gmail.com>
2021-11-03 09:23:58 +0700

    chore (calendar-events): create events empty state component

---
+++ components/Agenda/Calendar/Events/EmptyState/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-full flex flex-col items-center justify-center gap-4 md:w-[800px] md:h-[380px]">
+    <img src="/icons/empty-calendar.svg" alt="empty events" width="120" height="120">
+    <div class="w-full font-lato leading-6 text-center">
+      <strong class="text-base text-blue-gray-500">
+        Tidak ada kegiatan/event di hari ini
+      </strong>
+      <p class="text-xs text-gray-600">
+        Silahkan lihat ke tanggal
+        <span class="text-gray-800">sebelumnya</span> atau
+        <span class="text-gray-800">selanjutnya</span>
+      </p>
+    </div>
+  </div>
+</template>
