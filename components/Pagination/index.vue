39090d6ffaf26b50eb2e7e153c6cfdc2f64d613b
Ibwedagama <wardana.wedagama@gmail.com>
2021-11-04 13:51:26 +0700

    chore (pagination): create pagination component

---
+++ components/Pagination/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <jds-pagination
+    v-bind="$props"
+    @previous-page="$emit('previous-page', $event)"
+    @next-page="$emit('next-page', $event)"
+    @page-change="$emit('page-change', $event)"
+    @per-page-change="$emit('per-page-change', $event)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: [String, Number],
+      required: true
+    },
+    itemsPerPage: {
+      type: [String, Number],
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    totalRows: {
+      type: [String, Number],
+      required: true
+    },
+    itemsPerPageOptions: {
+      type: Array,
+      required: false,
+      default: () => [5, 10, 15, 20]
+    }
+  }
+}
+</script>
