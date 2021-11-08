accb4461371f839b6986cc764777c66d7fd3e3aa
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-11-08 15:20:46 +0700

    chore: add seperate agenda list

---
+++ components/Agenda/List/Item/index.vue
@@ -0,0 +1,320 @@
+<template>
+  <div class="flex gap-4">
+    <div class="relative left-0 text-sm text-blue-gray-200">
+      {{ startTime }}
+    </div>
+    <div class="flex gap-4 mb-4 w-full relative cursor-pointer">
+      <div>
+        <div class="relative pl-1">
+          <div
+            class="w-2 h-2 rounded-full relative z-10"
+            :class="circleClass"
+          />
+          <div
+            v-if="ongoing"
+            class="animate-ping w-2 h-2 rounded-full absolute z-10 top-0 border-2 border-green-800 bg-white"
+          />
+        </div>
+        <div
+          class="absolute border-l left-2"
+          :class="borderClass"
+          style="height: 112px; bottom: 88px"
+        />
+      </div>
+      <div
+        class="flex gap-2 w-full px-3 py-2 rounded-lg"
+        :class="cardClass"
+        @click="toggleEventDetail"
+      >
+        <div class="flex flex-col gap-2 w-full justify-center">
+          <p
+            class="line-clamp-1 text-sm font-roboto font-medium"
+            :class="titleClass"
+          >
+            {{ title }}
+          </p>
+          <p
+            class="line-clamp-1 text-xs px-2 py-1 rounded-md self-start"
+            :class="categoryClass"
+          >
+            {{ category }}
+          </p>
+          <div class="flex justify-between">
+            <p
+              class="line-clamp-1 text-xs whitespace-nowrap"
+              :class="timeClass"
+            >
+              {{ time }}
+            </p>
+            <p
+              class="line-clamp-1 text-xs whitespace-nowrap"
+              :class="statusClass"
+            >
+              {{ status }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Modal
+      :show="isEventDetailOpen"
+      :header="title"
+      :close-button="false"
+      @close="toggleEventDetail"
+    >
+      <div class="p-6 flex flex-col gap-6">
+        <div class="flex gap-6">
+          <div class="flex items-start gap-4">
+            <Icon src="/icons/agenda/category.svg" size="20px" />
+            <div class="flex flex-col gap-2">
+              <p class="text-xs text-blue-gray-200">
+                Kategori Acara
+              </p>
+              <p class="text-sm text-green-800 bg-green-50 px-2 py-1 rounded-md whitespace-nowrap">
+                {{ category }}
+              </p>
+            </div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <p class="text-xs text-blue-gray-200">
+              Tipe Acara
+            </p>
+            <p
+              class="text-sm px-2 py-1 rounded-md whitespace-nowrap capitalize"
+              :class="[ isOnline ? 'text-red-600 bg-red-100 ' : 'text-gray-600 bg-gray-100 ' ]"
+            >
+              {{ type }}
+            </p>
+          </div>
+        </div>
+        <div class="flex gap-6">
+          <div class="flex items-start gap-4">
+            <Icon src="/icons/agenda/calendar.svg" size="20px" />
+            <div class="flex flex-col gap-2">
+              <p class="text-xs text-blue-gray-200">
+                Tanggal
+              </p>
+              <p class="text-sm text-gray-800 whitespace-nowrap">
+                {{ formattedDate }}
+              </p>
+            </div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <p class="text-xs text-blue-gray-200">
+              Waktu
+            </p>
+            <p class="text-sm text-gray-800 whitespace-nowrap">
+              {{ time }}
+            </p>
+          </div>
+          <div class="text-xs text-blue-700 bg-blue-50 px-2 py-1 rounded-md whitespace-nowrap self-end">
+            {{ status }}
+          </div>
+        </div>
+        <div v-if="isOnline" class="flex gap-6">
+          <div class="flex items-start gap-4">
+            <Icon src="/icons/agenda/url.svg" size="20px" />
+            <div class="flex flex-col gap-2">
+              <p class="text-xs text-blue-gray-200">
+                Link Kegiatan
+              </p>
+              <Link :link="url" class="text-sm text-blue-600 underline">
+                {{ url }}
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div v-else class="flex gap-6">
+          <div class="flex items-start gap-4">
+            <Icon src="/icons/agenda/location.svg" size="20px" />
+            <div class="flex flex-col gap-2">
+              <p class="text-xs text-blue-gray-200">
+                Tempat Pelaksanaan
+              </p>
+              <p class="text-sm text-gray-800">
+                {{ address }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-50 py-4 flex justify-center items-center">
+        <div @click="toggleEventDetail">
+          <Button>Tutup</Button>
+        </div>
+      </div>
+    </Modal>
+  </div>
+</template>
+
+<script>
+import { format, getEachHour, hoursDifference, isCurrentHour, minutesDifference } from '~/utils/date'
+
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: false,
+      default: null
+    },
+    address: {
+      type: String,
+      required: false,
+      default: null
+    },
+    startHour: {
+      type: String,
+      required: true,
+      validator (value) {
+        /**
+         *  The value must match HH:mm:ss format
+         */
+        const pattern = /^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/
+        return pattern.test(value)
+      }
+    },
+    endHour: {
+      type: String,
+      required: true,
+      validator (value) {
+        /**
+         *  The value must match HH:mm:ss format
+         */
+        const pattern = /^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/
+        return pattern.test(value)
+      }
+    }
+  },
+  data () {
+    return {
+      isEventDetailOpen: false
+    }
+  },
+  computed: {
+    startTime () {
+      return this.startHour.substring(0, 5)
+    },
+    endTime () {
+      return this.endHour.substring(0, 5)
+    },
+    time () {
+      return `${this.startTime} - ${this.endTime} WIB`
+    },
+    done () {
+      const endHour = new Date(this.date).setHours(...this.endHour.split(':'))
+
+      const endHourDifferent = hoursDifference(endHour)
+      const endMinuteDifferent = minutesDifference(endHour)
+
+      return endHourDifferent >= 0 && endMinuteDifferent >= 0
+    },
+    ongoing () {
+      const startHour = new Date(this.date).setHours(...this.startHour.split(':'))
+      const endHour = new Date(this.date).setHours(...this.endHour.split(':'))
+      const eachHour = getEachHour({
+        start: startHour,
+        end: endHour
+      })
+
+      const currentHour = eachHour.map(hour => isCurrentHour(hour)).includes(true)
+      const startMinuteDifferent = minutesDifference(startHour)
+      const endMinuteDifferent = minutesDifference(endHour)
+
+      return currentHour && startMinuteDifferent >= 0 && endMinuteDifferent < 0
+    },
+    notStarted () {
+      return !this.done && !this.ongoing
+    },
+    status () {
+      if (this.done) {
+        return 'Telah selesai'
+      }
+
+      if (this.ongoing) {
+        return 'Sedang berlangsung'
+      }
+
+      return 'Belum dimulai'
+    },
+    formattedDate () {
+      return format(this.date, { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' })
+    },
+    isOnline () {
+      return this.type === 'online'
+    },
+    circleClass () {
+      return {
+        'bg-gray-300': this.notStarted,
+        'border-2 border-green-800 bg-white': this.ongoing,
+        'bg-green-800': this.done
+      }
+    },
+    borderClass () {
+      return {
+        'border-gray-300': this.notStarted,
+        'border-green-800': this.ongoing || this.done
+      }
+    },
+    cardClass () {
+      return {
+        'bg-gray-50 group hover:bg-green-50': this.notStarted,
+        'bg-green-800 bg-opacity-100': this.ongoing,
+        'bg-green-50 bg-opacity-50 border border-green-800': this.done
+      }
+    },
+    titleClass () {
+      return {
+        'group-hover:text-green-800': this.notStarted,
+        'text-white': this.ongoing,
+        'text-green-800': this.done
+      }
+    },
+    categoryClass () {
+      return {
+        'text-green-800 bg-gray-200 bg-opacity-75 group-hover:bg-green-100 group-hover:bg-opacity-40': this.notStarted,
+        'text-white bg-green-900 bg-opacity-50': this.ongoing,
+        'text-green-800 bg-green-100 bg-opacity-40': this.done
+      }
+    },
+    timeClass () {
+      return {
+        'text-gray-600': this.notStarted,
+        'text-green-800': this.done,
+        'text-white': this.ongoing
+      }
+    },
+    statusClass () {
+      return {
+        'text-gray-600': this.notStarted,
+        'text-green-800': this.done,
+        'text-white': this.ongoing
+      }
+    }
+  },
+  methods: {
+    toggleEventDetail () {
+      this.isEventDetailOpen = !this.isEventDetailOpen
+    }
+  }
+}
+</script>
--- components/Agenda/List/index.vue
+++ components/Agenda/List/index.vue
@@ -1,22 +1,41 @@
 <template>
   <div class="rounded-lg overflow-hidden border border-gray-300">
     <div class="p-6">
-      <AgendaWidgetList
-        with-time
-        :see-more="false"
-        :start-date="selectedDay"
-        :end-date="selectedDay"
-      >
-        <template #header>
-          <AgendaListHeader
-            :agenda-view="agendaView"
-            :each-day-of-week="eachDayOfWeek"
-            :selected-day="selectedDay"
-            :navigate="navigate"
-            @change="setSelectedDate"
-          />
+      <AgendaListHeader
+        :agenda-view="agendaView"
+        :each-day-of-week="eachDayOfWeek"
+        :selected-day="selectedDay"
+        :navigate="navigate"
+        @change="setSelectedDate"
+      />
+      <div class="bg-white flex flex-col overflow-hidden h-[700px]">
+        <!--
+          TODO: Show a placeholder if an error occur
+        -->
+        <template v-if="fetchState.pending">
+          <AgendaListSkeleton v-for="index in 10" :key="index" />
         </template>
-      </AgendaWidgetList>
+        <div v-else-if="!hasEvents" class="h-full px-4">
+          <AgendaEmptyState />
+        </div>
+        <div v-else class="flex flex-col overflow-y-scroll">
+          <InfiniteScroll :items="events" @refetch="getEvents">
+            <template #default="{ item }">
+              <AgendaListItem
+                :id="item.id"
+                :title="item.title"
+                :date="item.date"
+                :category="item.category"
+                :type="item.type"
+                :url="item.url"
+                :address="item.address"
+                :start-hour="item.start_hour"
+                :end-hour="item.end_hour"
+              />
+            </template>
+          </InfiniteScroll>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,11 +45,6 @@ import { formatISODate } from '~/utils/date'
 
 export default {
   props: {
-    calendarApi: {
-      type: Object,
-      required: false,
-      default: null
-    },
     agendaView: {
       type: String,
       required: true
@@ -48,17 +62,65 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      events: [],
+      fetchState: {
+        pending: false,
+        error: false
+      },
+      totalPage: 1,
+      perPage: 10
+    }
+  },
   computed: {
     selectedDay () {
       return formatISODate(this.selectedDate)
+    },
+    hasEvents () {
+      return this.events.length
+    }
+  },
+  watch: {
+    selectedDay () {
+      this.events = []
+      this.getEvents()
     }
   },
   deactivated () {
     this.$emit('change', this.selectedDay)
   },
+  mounted () {
+    this.getEvents()
+  },
   methods: {
     setSelectedDate (date) {
       this.$emit('change', date)
+    },
+    async getEvents (page = 1) {
+      if (page > this.totalPage) { return }
+
+      if (!this.events.length) {
+        this.fetchState.pending = true
+      }
+
+      const params = {
+        start_date: this.selectedDay,
+        end_date: this.selectedDay,
+        per_page: this.perPage,
+        page
+      }
+
+      try {
+        const response = await this.$axios.$get('/v1/events', { params })
+        this.events.push(...response.data)
+        this.totalPage = response.meta.total_page || 1
+
+        this.fetchState.pending = false
+      } catch (error) {
+        this.fetchState.error = true
+        this.fetchState.pending = false
+      }
     }
   }
 }
