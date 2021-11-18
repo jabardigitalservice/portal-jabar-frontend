159dc85a5815832d8acd7b55d971a0b7928bc911
Ibwedagama <wardana.wedagama@gmail.com>
2021-11-18 10:03:12 +0700

    chore (news-detail-tags): create news-detail-tags component

---
+++ components/NewsDetail/Tags/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="flex gap-4 items-center">
+    <span class="inline-flex items-center font-lato font-bold text-green-800 text-sm leading-6">
+      Tags
+      <Icon src="/icons/tags.svg" alt="tags" size="19px" class="ml-2" />
+    </span>
+    <!-- TODO: change the link to actual route -->
+    <nuxt-link
+      v-for="tag in tags"
+      :key="tag"
+      to="#"
+      class="rounded-full px-[10px] py-2 text-sm text-gray-700 bg-gray-200 lowercase"
+    >
+      {{ tag }}
+    </nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    tags: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
