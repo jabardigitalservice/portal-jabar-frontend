66155cc4ab30a8c0e465caa646d02048afea875b
Ibwedagama <wardana.wedagama@gmail.com>
2021-11-23 11:19:34 +0700

    chore (search-list): create search-list component

---
+++ components/Search/List/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="listView === 'list' ? 'w-full flex flex-col gap-8' : 'w-full grid grid-cols-3 gap-x-4 gap-y-8'">
+    <template v-if="loading">
+      <SearchItemSkeleton v-for="item in 5" :key="item" :view="listView" />
+    </template>
+    <template v-else>
+      <SearchItem v-for="item in 5" :key="item" :view="listView" />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    listView: {
+      type: String,
+      default: 'list',
+      validator (value) {
+        return ['list', 'grid'].includes(value)
+      }
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
