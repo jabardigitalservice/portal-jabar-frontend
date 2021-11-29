e9b1d935f591437c403a19d21436879f870feda3
Ibwedagama <wardana.wedagama@gmail.com>
2021-11-29 15:54:41 +0700

    feat (search): add pagination functionality; remove event listener on search bar

--- components/Search/index.vue
+++ components/Search/index.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseContainer class="relative -top-24 z-20">
     <div class="py-8 px-10 rounded-xl bg-white min-h-screen shadow">
-      <SearchBar class="mb-6" @search="fetchSearchResults" />
+      <SearchBar class="mb-6" />
       <div class="w-full h-full grid grid-cols-search-container gap-6">
         <section>
           <SearchSidebar />
@@ -36,19 +36,68 @@ export default {
         totalRows: 0,
         itemsPerPageOptions: [5, 10, 15]
       },
+      searchKeyword: null,
       searchData: [],
       searchMeta: []
     }
   },
+  watch: {
+    // watch `q` query param changes on the address bar
+    '$route.query.q': {
+      handler () {
+        if (this.$route.query.q) {
+          this.searchKeyword = this.$route.query.q
+          this.fetchSearchResults()
+        }
+      },
+      immediate: true
+    }
+  },
   methods: {
-    onPaginationChange (action) {
-      // TODO: Add action on pagination change
+    onPaginationChange (action, value) {
+      const paginationObj = { ...this.pagination }
+
+      switch (action) {
+        case 'prev-page':
+          paginationObj.currentPage = this.pagination.currentPage - 1
+          break
+        case 'next-page':
+          paginationObj.currentPage = this.pagination.currentPage + 1
+          break
+        case 'page-change':
+          paginationObj.currentPage = value
+          break
+        case 'per-page-change':
+          paginationObj.itemsPerPage = value
+          break
+        default:
+          break
+      }
+
+      this.pagination = JSON.parse(JSON.stringify(paginationObj))
+
+      /**
+       *  NOTE:
+       *  `jds-pagination` emits `page-change` and `per-page-change` events
+       *  whenever user changes per page value.
+       *
+       *  To avoid double fetch, we immediately stop this function on
+       *  `per-page-change` event and let `page-change` event to
+       *  fetch data from API
+       */
+      if (action === 'per-page-change') {
+        return
+      }
+
+      this.fetchSearchResults()
     },
-    async fetchSearchResults (value) {
+    async fetchSearchResults () {
       // TODO: Add search filter by category and sort by
       const params = {
+        q: this.searchKeyword,
         per_page: this.pagination.itemsPerPage,
-        q: value
+        page: this.pagination.currentPage,
+        sort_order: 'desc'
       }
 
       try {
@@ -57,6 +106,14 @@ export default {
         const { data, meta } = response.data
         this.searchData = data
         this.searchMeta = meta
+
+        const paginationObj = {
+          currentPage: this.searchMeta.current_page,
+          itemsPerPage: this.searchMeta.per_page,
+          totalRows: this.searchMeta.total_count
+        }
+
+        this.pagination = JSON.parse(JSON.stringify(paginationObj))
       } catch (error) {
         // silent error
         this.searchData = []
