4104aeb4c73619bfdecd8147c83cce552f77fcac
Bangun Bagustapa <bangunbagustapa@gmail.com>
2022-04-26 11:00:50 +0700

    feat: profile highlight banner

---
+++ components/Profile/Highlight/Banner/Background/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="absolute right-0 bottom-0">
+      <svg width="184" height="184" viewBox="0 0 184 184" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.5 183.5C0.500004 134.833 19.833 88.1588 54.2459 53.7459C88.6588 19.333 135.333 -5.80351e-07 184 0V58.526C150.855 58.526 119.067 71.6929 95.63 95.13C72.1929 118.567 59.026 150.355 59.026 183.5H0.5Z" fill="#008444" />
+      </svg>
+    </div>
+    <div class="absolute left-0 bottom-1/4">
+      <svg width="37" height="36" viewBox="0 0 37 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M36.5 18C36.5 27.9411 28.4411 36 18.5 36C8.55887 36 0.5 27.9411 0.5 18C0.5 8.05887 8.55887 0 18.5 0C28.4411 0 36.5 8.05887 36.5 18ZM9.73248 18C9.73248 22.8422 13.6578 26.7675 18.5 26.7675C23.3422 26.7675 27.2675 22.8422 27.2675 18C27.2675 13.1578 23.3422 9.23248 18.5 9.23248C13.6578 9.23248 9.73248 13.1578 9.73248 18Z" fill="#FFB900" />
+      </svg>
+    </div>
+    <div class="absolute left-4 top-4">
+      <svg width="134" height="135" viewBox="0 0 134 135" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0.5" width="133" height="135" rx="16" fill="#008444" />
+      </svg>
+    </div>
+  </div>
+</template>
---
+++ components/Profile/Highlight/Banner/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="min-h-[384px] bg-green-700 rounded-xl grid grid-cols-2 items-center gap-x-14">
+    <div class="pl-8 py-4">
+      <p class="font-lora font-bold text-[28px] text-white line-clamp-1">
+        Dibalik Layar
+      </p>
+      <h1 class="font-lora font-bold text-[40px] text-white mb-4 leading-[54px] line-clamp-2">
+        Provinsi Jawa Barat Yang Responsif
+      </h1>
+      <p class="text-white text-sm leading-[23px] mb-4 line-clamp-2">
+        Simak siapa saja yang turut andil dalam menjadikan Provinsi Jawa Barat, menjadi Provinsi yang responsif dan Juara Lahir Batin.
+      </p>
+      <Link link="#" tabindex="-1">
+        <Button class="banner-button" type="button" variant="primary">
+          Lihat di sini
+          <Icon name="open-new-tab" size="14px" />
+        </Button>
+      </Link>
+    </div>
+    <div class="relative w-full h-full px-12 py-14 overflow-hidden">
+      <ProfileHighlightBannerBackground />
+      <div
+        class="w-full h-full rounded-xl relative ml-4 bg-center bg-no-repeat bg-cover"
+        style="background-image: url('/images/banners/profile-highlight/behind-the-scene.webp')"
+      />
+    </div>
+  </section>
+</template>
+
+<style>
+.banner-button.jds-button--primary {
+  background-color: #FFFFFF;
+  color: #069550;
+}
+</style>
