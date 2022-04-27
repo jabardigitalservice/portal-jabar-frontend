f14f6db96d7e8323423b195d4bffc3f8cb98e4dc
Ibwedagama <wardana.wedagama@gmail.com>
2022-04-27 15:42:38 +0700

    feat (profile): add evelen priority section to profile/highlight component

--- components/Profile/Highlight/index.vue
+++ components/Profile/Highlight/index.vue
@@ -3,6 +3,7 @@
     <BaseContainer class="relative -top-24 z-20">
       <div class="py-8 px-10 rounded-xl bg-white min-h-screen flex flex-col gap-y-10">
         <ProfileHighlightBanner />
+        <ProfileHighlightElevenPriority />
         <ProfileHighlightContribution />
       </div>
     </BaseContainer>
