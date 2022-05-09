5b65276356c1884f9f378ad1777a561022a87268
Ibwedagama <wardana.wedagama@gmail.com>
2022-05-09 14:52:56 +0700

    chore (achievements): create jabar-achievements-list and item component

---
+++ components/Profile/JabarAchievements/Item/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <li
+    class="min-h-[158px] min-w-full p-4 rounded-xl flex border border-[#E9EDF4]"
+    :class="listView === 'list' ? 'gap-6' : 'flex-col gap-2'"
+  >
+    <img
+      :src="icon"
+      :alt="title"
+      class="w-[60px] h-[60px] object-cover"
+      width="60"
+      height="60"
+    >
+    <div>
+      <p class="font-lato font-bold text-sm text-blue-gray-800 leading-6 md:text-base">
+        {{ title }}
+      </p>
+      <p class="font-lato text-xs font-normal text-blue-gray-500 leading-5 mb-3 md:font-medium md:text-sm">
+        {{ appreciator }}
+      </p>
+      <p class="font-lato text-xs font-normal text-[#415C84] leading-5 line-clamp-3 mb-3">
+        {{ description }}
+      </p>
+      <nuxt-link
+        to="#"
+        class="rounded-md px-[10px] py-2 text-xs font-normal text-gray-700 bg-gray-100 cursor-pointer
+          hover:text-green-700 hover:bg-green-50"
+      >
+        {{ category }}
+      </nuxt-link>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    listView: {
+      type: String,
+      default: 'list'
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    appreciator: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    category: {
+      type: String,
+      default: ''
+    }
+  }
+
+}
+</script>
---
+++ components/Profile/JabarAchievements/List/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <ul :class="listView === 'list' ? 'flex flex-col gap-6' : 'grid grid-cols-2 gap-4'">
+    <ProfileJabarAchievementsItem
+      v-for="item in items"
+      :key="item.id"
+      :list-view="listView"
+      :title="item.title"
+      :appreciator="item.appreciator"
+      :category="item.category"
+      :description="item.description"
+      :icon="item.icon"
+    />
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    listView: {
+      type: String,
+      default: 'list'
+    }
+  },
+  data () {
+    return {
+      items: [
+        {
+          id: 1,
+          title: 'Anggota JDIHN terbaik tahun 2021',
+          appreciator: 'Menteri Hukum dan HAM RI',
+          category: 'Hukum dan HAM',
+          description: 'Jaringan Dokumentasi dan Informasi Hukum (JDIH) Jabar menjadi penyedia layanan publik di bidang dokumentasi dan informasi hukum terbaik di tingkat provinsi.',
+          icon: require('~/static/icons/selayang-pandang/achievement-idc-dx-awards.png')
+        },
+        {
+          id: 2,
+          title: 'Pembangunan Daerah Terbaik II',
+          appreciator: 'Bappenas RI',
+          category: 'Pembangunan',
+          description: 'Perencanaan pembangunan yang matang serta pencapaian yang telah diraih menuntun Jabar mendapatkan Penghargaan Pembangunan Daerah.',
+          icon: require('~/static/icons/selayang-pandang/icon-achievement-placeholder.svg')
+        },
+        {
+          id: 3,
+          title: 'Pemerintah Daerah Terpopuler',
+          appreciator: 'Anugerah Media Humas (AMH) 2021',
+          category: 'Hukum dan HAM',
+          description: 'Jaringan Dokumentasi dan Informasi Hukum (JDIH) Jabar menjadi penyedia layanan publik di bidang dokumentasi dan informasi hukum terbaik di tingkat provinsi.',
+          icon: require('~/static/icons/selayang-pandang/achievement-idc-dx-awards.png')
+        },
+        {
+          id: 4,
+          title: 'Best Governor',
+          appreciator: 'People Of The Year 2021',
+          category: 'Personal Gubernur',
+          description: 'Gubernur Ridwan Kamil dinobatkan sebagai Best Governor untuk dua kategori, Healthcare & Action Against Pandemi dan E-Goverment & Digital Innovation.',
+          icon: require('~/static/icons/selayang-pandang/icon-achievement-placeholder.svg')
+        },
+        {
+          id: 5,
+          title: 'Penghargaan Sumber Daya Energi & Mineral',
+          appreciator: 'Gatra Apresiasi Energi (GAE) 2021',
+          category: 'Sumber Daya Energi dan Mineral',
+          description: 'Jabar menerima apresiasi atas peran pemerintah daerah dalam memberikan dukungan kontinuitas bagi kemajuan usaha energi di lingkup regional',
+          icon: require('~/static/icons/selayang-pandang/achievement-idc-dx-awards.png')
+        }
+      ]
+    }
+  }
+
+}
+</script>
