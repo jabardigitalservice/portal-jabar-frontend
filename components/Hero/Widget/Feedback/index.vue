75ef6f9a4de1441ea68b3b5327098e33a6437d97
Ibwedagama <wardana.wedagama@gmail.com>
2022-05-19 14:57:14 +0700

    style (hero-feedback): adjust widget position

--- components/Hero/Widget/Feedback/index.vue
+++ components/Hero/Widget/Feedback/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="absolute h-[700px] top-0 -left-11 flex flex-col justify-center" @click="toggleFeedbackForm">
-      <div class="transform -rotate-90 group cursor-pointer">
+    <div class="absolute h-[700px] top-0 -left-11 flex flex-col" @click="toggleFeedbackForm">
+      <div class="relative top-[225px] transform -rotate-90 group cursor-pointer">
         <div class="bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm flex justify-center items-center gap-4 pl-3 pr-4 py-3 rounded-br-lg rounded-bl-lg border-l border-b border-transparent hover:bg-opacity-10 hover:border-white hover:border-opacity-10">
           <Icon src="/icons/feedback.svg" alt="Umpan balik" size="20px" class="transform rotate-90" />
           <p class="whitespace-nowrap text-white text-opacity-80 tracking-wide">
