0046b6b14dbf07dde625a2e70b149a7378e54ecd
Ibwedagama <wardana.wedagama@gmail.com>
2022-05-20 10:53:55 +0700

    refactor (footer): temporary hide element; refactor kotak-layanan link

--- components/Footer/Copyright/index.vue
+++ components/Footer/Copyright/index.vue
@@ -3,7 +3,8 @@
     <BaseContainer>
       <div class="flex flex-col items-center md:flex-row-reverse md:justify-between gap-5">
         <ul class="flex justify-between items-center gap-3 md:gap-4 text-center text-white">
-          <li>
+          <!-- NOTE: Temporary Hide Element -->
+          <!-- <li>
             <NuxtLink to="#">
               <a class="text-sm font-normal leading-6">
                 Ketentuan Layanan
@@ -18,13 +19,11 @@
               </a>
             </NuxtLink>
           </li>
-          <li class="h-6 w-1 border-r border-green-500" />
+          <li class="h-6 w-1 border-r border-green-500" /> -->
           <li>
-            <NuxtLink to="#">
-              <a class="text-sm font-normal leading-6">
-                Kontak Layanan
-              </a>
-            </NuxtLink>
+            <Link link="#" class="text-sm font-normal leading-6">
+              Kontak Layanan
+            </Link>
           </li>
         </ul>
         <p class="font-lato text-sm font-normal leading-6 text-white text-center md:text-left">
