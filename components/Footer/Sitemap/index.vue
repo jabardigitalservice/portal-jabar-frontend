bf5f9e83998ebf8dd6fe74aaf5bfb450ee2a2c26
Ida Bagus Wedagama <33661143+Ibwedagama@users.noreply.github.com>
2022-05-20 15:18:34 +0700

    revamp: footer (#200)
    * refactor (footer): temporary hide element; refactor kotak-layanan link
    
    * refactor (footer): change nuxt-link component with Link component
    
    * refactor (footer): add link to footer logo
    
    * refactor (footer): improve footer sitemap styling and structure

--- components/Footer/Sitemap/index.vue
+++ components/Footer/Sitemap/index.vue
@@ -1,63 +1,65 @@
 <template>
-  <div class="footer-sitemap flex w-full flex-col text-white">
+  <div class="flex w-full flex-col text-white">
     <div class="flex items-start gap-3">
       <Icon src="/icons/sitemap.svg" alt="Lokasi" size="18px" class="py-1" />
-      <div class="flex w-full flex-col gap-4">
+      <div class="flex w-full min-h-0 flex-col gap-4">
         <h2 class="font-roboto font-bold leading-7">
           Sitemap
         </h2>
+
         <!-- Sitemap Details (Mobile Only) -->
-        <details
-          v-for="navigation in navigationMenu"
-          :key="`details-${navigation.id}`"
-          class="footer-sitemap__details lg:hidden"
-        >
-          <summary class="flex justify-between items-center">
-            <h3 class="font-bold">
-              {{ navigation.title }}
-            </h3>
-            <div class="footer-sitemap__toggle-icon h-6 w-6 flex items-center justify-center rounded-full hover:bg-green-600 transition-all ease-in duration-150">
-              <Icon name="chevron-down" size="16px" fill="white" class="cursor-pointer" />
-            </div>
-          </summary>
-          <ul class="grid grid-cols-1 md:grid-cols-2 mt-2 gap-2">
-            <li v-for="item in navigation.items" :key="item.id">
-              <NuxtLink :to="item.link">
-                <a class="text-sm font-normal leading-6">
+        <section class="min-w-0 flex flex-col divide-y divide-green-500 -mt-4 lg:hidden">
+          <details
+            v-for="navigation in navigationMenu"
+            :key="`details-${navigation.id}`"
+            class="py-4"
+          >
+            <summary class="flex justify-between items-center">
+              <h3 class="font-bold">
+                {{ navigation.title }}
+              </h3>
+              <div class="h-6 w-6 flex items-center justify-center rounded-full hover:bg-green-600 transition-all ease-in duration-150">
+                <Icon name="chevron-down" size="16px" fill="white" class="cursor-pointer" />
+              </div>
+            </summary>
+            <ul class="grid grid-cols-1 md:grid-cols-2 mt-2 gap-2">
+              <li v-for="item in navigation.items" :key="item.id">
+                <Link :link="item.link" class="text-sm font-normal leading-6">
                   {{ item.title }}
-                </a>
-              </NuxtLink>
-            </li>
-          </ul>
-        </details>
+                </Link>
+              </li>
+            </ul>
+          </details>
+        </section>
+
         <!-- Sitemap Details (Large Screen)  -->
-        <div class="hidden lg:flex lg:justify-between lg:pr-20">
+        <section
+          class="min-w-0 w-11/12 hidden divide-x divide-green-500 justify-between
+            lg:flex lg:-ml-8 xl:-ml-16"
+        >
           <div
             v-for="navigation in navigationMenu"
             :key="`details-${navigation.id}`"
-            class="footer-sitemap__lg "
+            class="px-8 xl:px-16"
           >
             <h3 class="font-roboto text-base leading-6 font-bold mb-2">
               {{ navigation.title }}
             </h3>
             <ul class="grid grid-rows-4 grid-flow-col-dense gap-x-8 gap-y-2">
               <li v-for="item in navigation.items" :key="item.id" class="min-w-[95px]">
-                <NuxtLink :to="item.link">
-                  <a class="text-sm font-normal leading-6 whitespace-nowrap">
-                    {{ item.title }}
-                  </a>
-                </NuxtLink>
+                <Link :link="item.link" class="text-sm font-normal leading-6 whitespace-nowrap">
+                  {{ item.title }}
+                </Link>
               </li>
             </ul>
           </div>
-        </div>
+        </section>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 import { navigationMenu } from '~/static/data'
 
 export default {
@@ -68,19 +70,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.footer-sitemap .footer-sitemap__details + .footer-sitemap__details {
-  padding-top: 20px;
-  border-top: 1px solid #1FB767;
-}
-
-.footer-sitemap .footer-sitemap__lg:not(:first-child) {
-  border-left: 1px solid #1FB767;
-  padding-left: 32px;
-}
-
-.footer-sitemap__details[open] .footer-sitemap__toggle-icon {
-  transform: rotate(180deg);
-}
-</style>
--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -5,7 +5,9 @@
         class="py-6 md:py-12 flex flex-col gap-6 md:gap-12 bg-no-repeat"
         :style="{ backgroundImage: `url('/images/menu.svg')`, backgroundPosition: '100% 100%' }"
       >
-        <img src="/logo.svg" alt="Logo Portal Jabar" width="151" height="46">
+        <Link link="/" class="w-[fit-content]">
+          <img src="/logo.svg" alt="Logo Portal Jabar" width="151" height="46">
+        </Link>
         <FooterInfo />
         <FooterSitemap />
       </div>
