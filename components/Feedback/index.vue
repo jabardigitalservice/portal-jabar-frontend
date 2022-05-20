bf44b2c19d714e3c5a9df4224b968d4e56e4a759
Ida Bagus Wedagama <33661143+Ibwedagama@users.noreply.github.com>
2022-05-20 10:09:29 +0700

    refactor: feedback form (#198)
    * refactor (button): add listeners on Button component
    
    * refactor (feedback): refactor feedback modal using base-modal; remove form tag
    
    * refactor (feedback): remove multiple feedback modal; add dialog state

--- components/Feedback/index.vue
+++ components/Feedback/index.vue
@@ -1,124 +1,122 @@
 <template>
   <div>
-    <Modal
-      :show="show"
-      header="Seberapa Puas Anda dengan Portal Jabar"
-      @click="closeFeedbackForm"
-    >
-      <form @submit.prevent="submitHandler">
-        <div class="p-6">
-          <div class="flex flex-col mb-5">
-            <label class="text-sm leading-6 text-gray-800 mb-2">
-              Seberapa Puas Anda dengan Portal Jabar
-            </label>
-            <div class="flex gap-7">
-              <JdsRadioButtonGroup
-                :items="ratingOptions"
-                placeholder-key="label"
-                name="radio-button-group"
-                orientation="horizontal"
-                @change="setFormRating"
-              />
-            </div>
-          </div>
-          <div class="flex flex-col mb-5">
-            <label id="compliments" class="text-sm leading-6 text-gray-800 mb-2">
-              Apa yang menurut Anda paling bermanfaat dari Portal Jabar?
-            </label>
-            <textarea
-              id="compliments"
-              v-model="form.compliments"
-              name="compliments"
-              cols="30"
-              rows="3"
-              class="bg-gray-50 rounded-lg p-2 text-sm mb-1"
-              placeholder="Masukkan jawaban Anda di sini"
-              maxlength="1500"
+    <!-- Feedback Form -->
+    <BaseModal :show="show">
+      <!-- Form Header -->
+      <template #header>
+        <h1 class="font-roboto font-medium text-[21px] leading-[34px] text-green-700 px-6 py-2">
+          Seberapa Puas Anda dengan Portal Jabar
+        </h1>
+      </template>
+      <!-- Form Body -->
+      <div class="p-6 overflow-y-auto">
+        <div class="flex flex-col mb-5">
+          <label class="text-sm leading-6 text-gray-800 mb-2">
+            Seberapa Puas Anda dengan Portal Jabar
+          </label>
+          <div class="flex gap-7">
+            <JdsRadioButtonGroup
+              :items="ratingOptions"
+              placeholder-key="label"
+              name="radio-button-group"
+              orientation="horizontal"
+              @change="setFormRating"
             />
-            <small class="text-gray-600">
-              {{ getRemainingCharacters('compliments') }} karakter tersisa
-            </small>
-          </div>
-          <div class="flex flex-col mb-5">
-            <label id="criticism" class="text-sm leading-6 text-gray-800 mb-2">
-              Apa yang perlu diperbaiki dari Portal Jabar?
-            </label>
-            <textarea
-              id="criticism"
-              v-model="form.criticism"
-              name="criticism"
-              cols="30"
-              rows="3"
-              class="bg-gray-50 rounded-lg p-2 text-sm mb-1"
-              placeholder="Masukkan jawaban Anda di sini"
-              maxlength="1500"
-            />
-            <small class="text-gray-600">
-              {{ getRemainingCharacters('criticism') }} karakter tersisa
-            </small>
-          </div>
-          <div class="flex flex-col mb-5">
-            <label id="suggestions" class="text-sm leading-6 text-gray-800 mb-2">
-              Apakah yang perlu Portal Jabar sediakan untuk kebutuhan pengguna?
-            </label>
-            <textarea
-              id="suggestions"
-              v-model="form.suggestions"
-              name="suggestions"
-              cols="30"
-              rows="3"
-              class="bg-gray-50 rounded-lg p-2 text-sm mb-1"
-              placeholder="Masukkan jawaban Anda di sini"
-              maxlength="1500"
-            />
-            <small class="text-gray-600">
-              {{ getRemainingCharacters('suggestions') }} karakter tersisa
-            </small>
           </div>
         </div>
+        <div class="flex flex-col mb-5">
+          <label id="compliments" class="text-sm leading-6 text-gray-800 mb-2">
+            Apa yang menurut Anda paling bermanfaat dari Portal Jabar?
+          </label>
+          <textarea
+            id="compliments"
+            v-model="form.compliments"
+            name="compliments"
+            cols="30"
+            rows="3"
+            class="bg-gray-50 rounded-lg p-2 text-sm mb-1"
+            placeholder="Masukkan jawaban Anda di sini"
+            maxlength="1500"
+          />
+          <small class="text-gray-600">
+            {{ getRemainingCharacters('compliments') }} karakter tersisa
+          </small>
+        </div>
+        <div class="flex flex-col mb-5">
+          <label id="criticism" class="text-sm leading-6 text-gray-800 mb-2">
+            Apa yang perlu diperbaiki dari Portal Jabar?
+          </label>
+          <textarea
+            id="criticism"
+            v-model="form.criticism"
+            name="criticism"
+            cols="30"
+            rows="3"
+            class="bg-gray-50 rounded-lg p-2 text-sm mb-1"
+            placeholder="Masukkan jawaban Anda di sini"
+            maxlength="1500"
+          />
+          <small class="text-gray-600">
+            {{ getRemainingCharacters('criticism') }} karakter tersisa
+          </small>
+        </div>
+        <div class="flex flex-col mb-5">
+          <label id="suggestions" class="text-sm leading-6 text-gray-800 mb-2">
+            Apakah yang perlu Portal Jabar sediakan untuk kebutuhan pengguna?
+          </label>
+          <textarea
+            id="suggestions"
+            v-model="form.suggestions"
+            name="suggestions"
+            cols="30"
+            rows="3"
+            class="bg-gray-50 rounded-lg p-2 text-sm mb-1"
+            placeholder="Masukkan jawaban Anda di sini"
+            maxlength="1500"
+          />
+          <small class="text-gray-600">
+            {{ getRemainingCharacters('suggestions') }} karakter tersisa
+          </small>
+        </div>
+      </div>
+      <!-- Form Footer -->
+      <template #footer>
         <div class="bg-gray-50 py-4 px-6 flex justify-end items-center gap-4">
-          <!--
-            TODO: Replace button with button component from jabar design system
-          -->
-          <button
-            type="button"
-            class="border border-green-700 text-green-700 rounded-lg px-4 py-2 font-bold text-sm leading-4 hover:bg-green-50"
+          <Button
+            variant="secondary"
             @click="closeFeedbackForm"
           >
             Tutup
-          </button>
+          </Button>
           <Button
-            type="submit"
             :disabled="!isValid"
             :style="[ !isValid && { backgroundColor: '#E0E0E0', color: '#FFFFFF' } ]"
             :loading="isLoading"
+            @click="submitHandler"
           >
             Kirim Survey
           </Button>
         </div>
-      </form>
-    </Modal>
-    <Modal
-      :show="openedModal === 'success'"
-      header="Terima kasih"
-      body="Feedback Anda telah kami terima. Masukan Anda akan menjadi acuan kami untuk pengembangan Portal Jabar ke arah yang lebih baik."
-      label="OK"
-      @click="closeModal"
-    />
-    <Modal
-      :show="openedModal === 'not-allowed'"
-      header="Mohon Maaf"
-      body="Kami melihat bahwa Anda telah mengirimkan feedback baru-baru ini. Silakan kembali untuk mecoba mengirimkan feedback dalam kurun waktu 1 Jam (60 menit)."
-      label="OK, saya mengerti"
-      @click="closeModal"
-    />
-    <Modal
-      :show="openedModal === 'error'"
-      header="Mohon Maaf"
-      body="Terjadi kesalahan pada sistem kami. Silakan coba kembali beberapa saat lagi."
-      label="OK, saya mengerti"
-      @click="closeModal"
-    />
+      </template>
+    </BaseModal>
+
+    <!-- Dialog -->
+    <BaseModal
+      :show="dialog.open"
+      :button-label="dialog.buttonLabel"
+      @close="closeModal"
+    >
+      <template #header>
+        <h1 class="font-roboto font-medium text-[21px] leading-[34px] text-green-700 px-6 py-2">
+          {{ dialog.title }}
+        </h1>
+      </template>
+      <div class="max-w-[500px] py-4 px-6">
+        <p class="text-sm text-gray-800">
+          {{ dialog.body }}
+        </p>
+      </div>
+    </BaseModal>
   </div>
 </template>
 
@@ -149,7 +147,12 @@ export default {
         { label: '4', value: 4 },
         { label: '5', value: 5 }
       ],
-      openedModal: ''
+      dialog: {
+        open: false,
+        title: '',
+        body: '',
+        buttonLabel: ''
+      }
     }
   },
   computed: {
@@ -179,6 +182,7 @@ export default {
     closeModal () {
       this.closeFeedbackForm()
       this.resetFormInput()
+      this.resetDialog()
       this.openedModal = ''
     },
     resetFormInput () {
@@ -187,6 +191,14 @@ export default {
         compliments: '',
         criticism: '',
         suggestions: ''
+      }
+    },
+    resetDialog () {
+      this.dialog = {
+        open: false,
+        title: '',
+        body: '',
+        buttonLabel: ''
       }
     },
     isAllowed () {
@@ -206,7 +218,12 @@ export default {
     },
     async submitHandler () {
       if (!this.isAllowed()) {
-        this.openedModal = 'not-allowed'
+        this.dialog = {
+          open: true,
+          title: 'Mohon Maaf',
+          body: 'Kami melihat bahwa Anda telah mengirimkan feedback baru-baru ini. Silakan kembali untuk mecoba mengirimkan feedback dalam kurun waktu 1 Jam (60 menit).',
+          buttonLabel: 'OK, saya mengerti'
+        }
       } else {
         const form = {
           rating: this.form.rating,
@@ -218,11 +235,21 @@ export default {
           this.isLoading = true
           await this.$axios.$post('/v1/feedback', form)
           this.isLoading = false
-          this.openedModal = 'success'
+          this.dialog = {
+            open: true,
+            title: 'Terima kasih',
+            body: 'Feedback Anda telah kami terima. Masukan Anda akan menjadi acuan kami untuk pengembangan Portal Jabar ke arah yang lebih baik.',
+            buttonLabel: 'OK'
+          }
           this.setLocalStorage('feedback', new Date())
         } catch (error) {
           this.isLoading = false
-          this.openedModal = 'error'
+          this.dialog = {
+            open: true,
+            title: 'Mohon Maaf',
+            body: 'Terjadi kesalahan pada sistem kami. Silakan coba kembali beberapa saat lagi.',
+            buttonLabel: 'OK, saya mengerti'
+          }
         }
       }
     }
