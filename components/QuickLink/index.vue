41981c0bf9948d5e55b6be1502b2b72db7e68354
Ibwedagama <wardana.wedagama@gmail.com>
2022-06-15 16:41:12 +0700

    style (quicklink): add responsive style for mobile and tablet viewoprt

--- components/QuickLink/index.vue
+++ components/QuickLink/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="bg-white p-16">
+  <section class="bg-white py-4 md:p-4 xl:p-16">
     <BaseContainer>
-      <ul class="grid grid-cols-3 grid-rows-3 gap-x-20 gap-y-12">
-        <li class="flex flex-col gap-2">
-          <h2 class="text-4xl font-medium leading-loose">
+      <ul class="grid md:grid-cols-3 md:grid-rows-[auto,1fr,1fr] lg:grid-rows-3 md:gap-x-6 lg:gap-x-20 gap-y-6 lg:gap-y-12">
+        <li class="md:row-start-1 md:col-start-1 md:col-end-4 lg:row-start-auto lg:col-start-auto lg:col-end-auto flex flex-col gap-2 lg:gap-6">
+          <h2 class="text-2xl text-center lg:text-left md:text-4xl font-medium leading-loose">
             Akses Cepat
           </h2>
-          <p class="text-sm text-gray-600 leading-6">
+          <p class="text-center lg:text-left text-sm text-gray-600 leading-6">
             Dapatkan kemudahaan akses ke beberapa layanan Pemerintah Provinsi Jawa Barat untuk kebutuhan Anda.
           </p>
         </li>
