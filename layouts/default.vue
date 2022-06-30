9b02bff8467cb1aae0e22834f660a36e7a3f1390
Ibwedagama <wardana.wedagama@gmail.com>
2022-06-30 22:30:30 +0700

    refactor (layouts): add modal transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <PortalTarget name="modal" />
+    <PortalTarget
+      name="modal"
+      slim
+      :transition="modalTransition"
+    />
     <Header />
     <div class="relative">
       <LazyGPRWidget v-if="!device.isMobile" />
@@ -16,7 +20,37 @@ export default {
   computed: {
     device () {
       return this.$store.state.device.device
+    },
+    modalTransition () {
+      return {
+        functional: true,
+        render (h, context) {
+          return h('transition', { props: { name: 'modal', mode: 'out-in' } }, context.children)
+        }
+      }
     }
   }
 }
 </script>
+
+<style>
+.modal-enter-active,
+.modal-leave-active {
+  @apply transition-opacity ease-brand duration-250 opacity-100;
+}
+
+.modal-enter,
+.modal-leave-to {
+  @apply opacity-0;
+}
+
+.modal-enter-active .modal__body,
+.modal-leave-active .modal__body {
+  @apply transition-transform ease-brand duration-300;
+}
+
+.modal-enter .modal__body,
+.modal-leave-to .modal__body {
+  transform: translateY(200px);
+}
+</style>
