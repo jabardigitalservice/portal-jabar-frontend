caed1fb62052d8091f80b3530a0e75207b591c09
Ibwedagama <wardana.wedagama@gmail.com>
2022-07-02 14:29:42 +0700

    refactor (base-modal): add bottom sheet on mobile viewport

--- components/Base/Modal/index.vue
+++ components/Base/Modal/index.vue
@@ -3,31 +3,45 @@
     <div
       v-if="show"
       ref="modal"
-      class="fixed w-full h-full inset-0 bg-black bg-opacity-40 backdrop-filter backdrop-blur-sm z-[100]"
+      class="fixed w-full h-full inset-0 bg-black bg-opacity-40 backdrop-filter backdrop-blur-sm z-[100] md:flex md:justify-center md:items-center"
     >
-      <div class="w-full h-full md:flex md:justify-center md:items-center overflow-y-auto md:overflow-hidden">
-        <div
-          class="modal__body relative top-[25%] flex flex-col min-h-[75%] max-h-screen max-w-full bg-white rounded-xl
-          md:static md:min-h-0 md:max-h-[90%] md:max-w-[640px] lg:max-w-[800px] md:overflow-hidden"
-        >
-          <div class="md:hidden absolute w-full flex justify-end px-4 -top-14">
-            <button class="h-10 w-10 bg-green-800 flex items-center justify-center rounded-full" @click="$emit('close')">
-              <Icon name="times" size="16px" class="text-white" />
-            </button>
-          </div>
-          <div class=" md:hidden w-full flex justify-center py-4">
-            <div class="w-24 h-[4px] rounded-full bg-gray-300" />
-          </div>
-          <slot name="header" />
-          <slot />
-          <slot name="footer">
-            <div class="bg-white flex w-full items-center justify-center py-4 z-[100] mt-auto px-6">
-              <Button class="w-full md:w-auto !justify-center" @click="$emit('close')">
-                {{ buttonLabel }}
-              </Button>
-            </div>
-          </slot>
+      <!-- Bottom Sheet (Mobile Only) -->
+      <div
+        v-if="device.isMobile"
+        class="modal__body relative flex flex-col max-w-full bg-white rounded-xl h-full transition-all ease-brand duration-300"
+        :class="fullScreen ? 'top-0 min-h-full' : 'min-h-[75%] max-h-[75%] top-[25%]'"
+      >
+        <div v-touch:swipe.self="handleSwipe" class="absolute w-full h-20 top-0" />
+        <div class="absolute w-full flex justify-end px-4 -top-14">
+          <button class="h-10 w-10 bg-green-800 flex items-center justify-center rounded-full" @click="closeModal">
+            <Icon name="times" size="16px" class="text-white" />
+          </button>
         </div>
+        <div class="p-4">
+          <div class="w-24 h-[4px] rounded-full bg-gray-300 mx-auto" />
+        </div>
+        <slot name="header" />
+        <slot class="overflow-y-auto" />
+        <slot name="footer">
+          <div class="bg-white flex w-full items-center justify-center py-4 z-[100] mt-auto px-6">
+            <Button class="w-full !justify-center" @click="closeModal">
+              {{ buttonLabel }}
+            </Button>
+          </div>
+        </slot>
+      </div>
+
+      <!-- Default Modal (Tablet and Dekstop) -->
+      <div v-else class="modal__body flex flex-col bg-white rounded-lg overflow-hidden max-h-[90%] max-w-[800px]">
+        <slot name="header" />
+        <slot />
+        <slot name="footer">
+          <div class="bg-white flex w-full items-center justify-center py-4 z-[100]">
+            <Button type="button" @click="closeModal">
+              {{ buttonLabel }}
+            </Button>
+          </div>
+        </slot>
       </div>
     </div>
   </Portal>
@@ -48,9 +62,45 @@ export default {
       default: ''
     }
   },
+  data () {
+    return {
+      fullScreen: false
+    }
+  },
+  computed: {
+    device () {
+      return this.$store.state.device.device
+    }
+  },
   watch: {
     show () {
       lockScroll(this.show)
+    }
+  },
+  methods: {
+    handleSwipe (direction) {
+      switch (direction) {
+        case 'top':
+          if (!this.fullScreen) {
+            this.fullScreen = true
+          }
+          break
+
+        case 'bottom':
+          if (this.fullScreen) {
+            this.fullScreen = false
+          } else {
+            this.$emit('close')
+          }
+          break
+
+        default:
+          break
+      }
+    },
+    closeModal () {
+      this.fullScreen = false
+      this.$emit('close')
     }
   }
 }
