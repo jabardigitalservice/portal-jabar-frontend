9abdca49ed1a1218a19687e9023e452f88cf40ab
Ibwedagama <wardana.wedagama@gmail.com>
2022-07-02 19:20:50 +0700

    fix (layouts): remove caching exclude

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
     <Header />
     <div class="relative">
       <LazyGPRWidget v-if="!device.isMobile" />
-      <Nuxt keep-alive :keep-alive-props="{ max: 5, exclude: ['HomePage'] }" />
+      <Nuxt keep-alive :keep-alive-props="{ max: 5 }" />
     </div>
     <Footer />
     <!-- <MobileViewportBlocker /> -->
