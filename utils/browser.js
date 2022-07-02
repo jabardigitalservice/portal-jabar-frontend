9a12bfec1cee0f2f793bb6e25b5d6fa568ba93a8
Ibwedagama <wardana.wedagama@gmail.com>
2022-07-02 14:27:35 +0700

    refactor (plugin/browser): add overscroll contain to body

--- utils/browser.js
+++ utils/browser.js
@@ -10,12 +10,14 @@ export function lockScroll (bool) {
     document.body.style.width = '100%'
     document.body.style.height = '100%'
     document.body.style.overflowY = 'scroll'
+    document.body.style.overscrollBehavior = 'contain'
   } else {
     const scrollY = document.body.style.top
     document.body.style.position = ''
     document.body.style.width = ''
     document.body.style.height = ''
     document.body.style.overflowY = ''
+    document.body.style.overscrollBehavior = ''
     document.body.style.top = ''
     window.scrollTo(0, parseInt(scrollY || '0') * -1)
   }
