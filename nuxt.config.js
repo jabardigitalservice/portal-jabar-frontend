5c50621f3bce0d649e6a761d8b20b71fece0eb00
Ibwedagama <wardana.wedagama@gmail.com>
2022-07-06 10:14:55 +0700

    fix (nuxt.config): remove meta maximum-scale to improve accessibility

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Portal ini menyediakan informasi resmi, akses ke berbagai layanan publik, dan tempat menyampaikan aspirasi mengenai program dari Pemerintah Daerah Provinsi Jawa Barat.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
