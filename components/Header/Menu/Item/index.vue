cf662706e36cf3170eb6e22c59f85d284484e1bf
Ibwedagama <wardana.wedagama@gmail.com>
2022-07-14 13:03:40 +0700

    style (header): add animation on great menu

--- components/Header/Menu/Item/index.vue
+++ components/Header/Menu/Item/index.vue
@@ -1,67 +1,65 @@
 <template>
-  <ul v-show="isMenuActive" class="bg-green-primary w-full absolute top-full left-0 border-t border-green-400">
-    <BaseContainer>
-      <div
-        class="flex mx-auto items-start pt-6 pb-10 bg-no-repeat"
-        :style="{ backgroundImage: `url('/images/menu.svg')`, backgroundPosition: '100% 100%' }"
-      >
-        <div class="flex flex-col gap-8 flex-grow">
-          <li>
-            <Link :link="link" class="text-white font-roboto font-medium text-3xl leading-10" @click="setActiveMenu">
-              {{ title }}
-            </Link>
-          </li>
-          <li>
-            <ul
-              class="grid grid-cols-3 grid-rows-2 gap-y-6 gap-x-20"
-              :class="{ 'grid-flow-col': items.length <= 3}"
-            >
-              <li v-for="item in items" :key="item.id">
-                <Link v-if="item.icon" :link="item.link" class="flex gap-4 items-start" @click="setActiveMenu">
-                  <img class="py-1.5" :src="item.icon">
-                  <div>
-                    <h4 class="text-lg font-bold text-gray-50 whitespace-nowrap">
-                      {{ item.title }}
-                    </h4>
-                    <p class="text-sm text-gray-50 font-roboto opacity-80">
-                      {{ item.description }}
-                    </p>
-                  </div>
-                </Link>
-                <ul v-else class="flex gap-4 w-80 items-start">
-                  <div>
-                    <li v-if="item.title" class="text-lg font-roboto font-bold text-gray-50 uppercase mb-4">
-                      <Link :link="item.link" @click="setActiveMenu">
+  <transition name="fade" mode="out-in">
+    <ul v-show="isMenuActive" class="bg-green-primary w-full absolute top-full left-0 border-t border-green-400">
+      <BaseContainer>
+        <div
+          class="flex mx-auto items-start pt-6 pb-10 bg-no-repeat"
+          :style="{ backgroundImage: `url('/images/menu.svg')`, backgroundPosition: '100% 100%' }"
+        >
+          <div class="flex flex-col gap-8 flex-grow">
+            <li>
+              <Link :link="link" class="text-white font-roboto font-medium text-3xl leading-10" @click="setActiveMenu">
+                {{ title }}
+              </Link>
+            </li>
+            <li>
+              <ul
+                class="grid grid-cols-3 grid-rows-2 gap-y-6 gap-x-20"
+                :class="{ 'grid-flow-col': items.length <= 3}"
+              >
+                <li v-for="item in items" :key="item.id">
+                  <Link v-if="item.icon" :link="item.link" class="flex gap-4 items-start" @click="setActiveMenu">
+                    <img class="py-1.5" :src="item.icon">
+                    <div>
+                      <h4 class="text-lg font-bold text-gray-50 whitespace-nowrap">
                         {{ item.title }}
-                      </Link>
-                    </li>
-                    <ul v-if="item.items.length" class="flex flex-col gap-4">
-                      <li v-for="subItem in item.items" :key="subItem.id">
-                        <Link :link="subItem.link" class="text-lg text-gray-50 font-roboto opacity-80" @click="setActiveMenu">
-                          {{ subItem.title }}
+                      </h4>
+                      <p class="text-sm text-gray-50 font-roboto opacity-80">
+                        {{ item.description }}
+                      </p>
+                    </div>
+                  </Link>
+                  <ul v-else class="flex gap-4 w-80 items-start">
+                    <div>
+                      <li v-if="item.title" class="text-lg font-roboto font-bold text-gray-50 uppercase mb-4">
+                        <Link :link="item.link" @click="setActiveMenu">
+                          {{ item.title }}
                         </Link>
                       </li>
-                    </ul>
-                  </div>
-                </ul>
-              </li>
-            </ul>
-          </li>
+                      <ul v-if="item.items.length" class="flex flex-col gap-4">
+                        <li v-for="subItem in item.items" :key="subItem.id">
+                          <Link :link="subItem.link" class="text-lg text-gray-50 font-roboto opacity-80" @click="setActiveMenu">
+                            {{ subItem.title }}
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </div>
+          <div class="cursor-pointer" tabindex="0" @click.stop="setActiveMenu">
+            <Icon name="times" size="24px" class="text-white" />
+          </div>
         </div>
-        <div class="cursor-pointer" tabindex="0" @click.stop="setActiveMenu">
-          <Icon name="times" size="24px" class="text-white" />
-        </div>
-      </div>
-    </BaseContainer>
-  </ul>
+      </BaseContainer>
+    </ul>
+  </transition>
 </template>
 
 <script>
 export default {
-  transition: {
-    name: 'home',
-    mode: 'out-in'
-  },
   props: {
     id: {
       type: Number,
@@ -97,3 +95,14 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  @apply transition-opacity ease-brand duration-250 opacity-100;
+}
+
+.fade-enter {
+  @apply opacity-0;
+}
+</style>
